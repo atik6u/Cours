4ce4ef6f416566e9763a9252c97070fbb9901c2b
Mohammed ATIK <mohammed.atik8@etu.univ-lorraine.fr>
2020-10-05 18:07:46 +0200

    Commit projet Demo29Sep

---
+++ Demo29Sep/src/fr/polytech/systeme/Test.java
@@ -0,0 +1,61 @@
+package fr.polytech.systeme;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("<h1>Served at: ").append(request.getContextPath()).append("</h1>");
+		/*response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("</head>");
+
+		out.println("</head>");
+		out.println("<title> demoCours </title>");
+		out.println("<body>");
+		out.println("<h1> Hello !!!!! </h1>");
+		out.println("</body>");
+		out.println("</html>");
+		*/
+		this.getServletContext().getRequestDispatcher("/WEB-INF/coucou.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
